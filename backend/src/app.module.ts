3b45ca3bb5cf7c0aa608f5872a6a280afd7b18e3
Geferson Lopes <gefersonjefreey@gmail.com>
2025-10-02 19:00:22 -0300

    chore: configurações globais e saúde da aplicação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { CompaniesModule } from './modules/companies/companies.module';
 import { EmployeesModule } from './modules/employees/employees.module';
 import { AssetsModule } from './modules/assets/assets.module';
+import { HealthModule } from './health/health.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CompaniesModule, EmployeesModule, AssetsModule],
+  imports: [
+    CompaniesModule,
+    EmployeesModule,
+    AssetsModule,
+    DatabaseModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Forte Asset Manager')
+    .setDescription('API RESTful para gestão de ativos de TI')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 
 bootstrap()

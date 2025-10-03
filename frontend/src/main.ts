a29ac402415218fd46e422caa1b8080485bb0d0f
Geferson Lopes <gefersonjefreey@gmail.com>
2025-10-03 17:07:56 -0300

    chore(frontend): configurações globais

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/routes/app.routes';
+import { RootComponent } from './app/core/root.component';
+import { apiHttpInterceptor } from './app/core/interceptors/api-http.interceptor';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(RootComponent, {
+  providers: [provideHttpClient(withInterceptors([apiHttpInterceptor])), provideRouter(routes)],
+}).catch(console.error);
